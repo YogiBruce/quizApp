3938419e57ada16c136bcf365d69e4b605fb7039
YogiBruce <108956598+YogiBruce@users.noreply.github.com>
2022-09-11 13:01:05 -0700

    questions and styles

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,51 +16,51 @@ let availableQuestions = [];
 //QUESTIONS ARRAY
 let questions = [
     {
-        question: "LOREM IPSUM?",
-        choice1: "lorem ipsum",
-        choice2: "ipsum lorem",
-        choice3: "lorum ipsem",
-        choice4: "lorip emsum",
+        question: "Who is not one of the original 9 members of Wu-Tang?",
+        choice1: "Inspectah Deck",
+        choice2: "Cappadonna",
+        choice3: "Ghostface Killah",
+        choice4: "Raekwon",
         answer: 2
     },
     {
-        question: "LOREM IPSUM?",
-        choice1: "lorem ipsum",
-        choice2: "ipsum lorem",
-        choice3: "lorum ipsem",
-        choice4: "lorip emsum",
-        answer: 2
-    },
-    {
-        question: "LOREM IPSUM?",
-        choice1: "lorem ipsum",
-        choice2: "ipsum lorem",
-        choice3: "lorum ipsem",
-        choice4: "lorip emsum",
+        question: "Which borough of New York city is Wu-Tang from?",
+        choice1: "The Bronx",
+        choice2: "Queens",
+        choice3: "Staten Island",
+        choice4: "Brooklyn",
         answer: 3
     },
     {
-        question: "LOREM IPSUM?",
-        choice1: "lorem ipsum",
-        choice2: "ipsum lorem",
-        choice3: "lorum ipsem",
-        choice4: "lorip emsum",
+        question: "What is the name of the club that Wu-Tang rushed the stage to drop their first single, \"Protect Ya Neck\"?",
+        choice1: "Tunnel",
+        choice2: "Palladium",
+        choice3: "Club Expo",
+        choice4: "The Fever",
+        answer: 4
+    },
+    {
+        question: "Which Wu-Tang member recorded lyrics for the album The W from a jail telephone while incarcerated?",
+        choice1: "RZA",
+        choice2: "ODB",
+        choice3: "UGod",
+        choice4: "GZA",
         answer: 2
     },
     {
-        question: "LOREM IPSUM?",
-        choice1: "lorem ipsum",
-        choice2: "ipsum lorem",
-        choice3: "lorum ipsem",
-        choice4: "lorip emsum",
-        answer: 2
+        question: "Who is the only commercially acclaimed 90's West Coast artist to be featured on a Wu-Tang track?",
+        choice1: "Snoop Dogg",
+        choice2: "Dr. Dre",
+        choice3: "Ice Cube",
+        choice4: "2Pac",
+        answer: 1
     },
     {
-        question: "LOREM IPSUM?",
-        choice1: "lorem ipsum",
-        choice2: "ipsum lorem",
-        choice3: "lorum ipsem",
-        choice4: "lorip emsum",
+        question: "Method Man proclaims \"Hey you! Get off my cloud!\" in his self-titled song in a nod to which famous 70\'s Rock-n-Roll band?",
+        choice1: "The Who",
+        choice2: "Pink Floyd",
+        choice3: "The Rolling Stones",
+        choice4: "Led Zeppelin",
         answer: 3
     },
 ]
@@ -72,7 +72,7 @@ const MAX_QUESTIONS = 6;
 function startGame(){
     questionCounter =0;
     score = 0;
-    timerCount = 30;
+    timerCount = 45;
     availableQuestions = [...questions];
     getNewQuestion();
 };
